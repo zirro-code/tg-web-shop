fdbe52e998b05cb7f530b3f0a6f1df9d8b744f81
Anton <109826223+Arcadiyyyyyyyy@users.noreply.github.com>
2025-05-27 10:59:56 +0000

    feat: add user to db

--- tgbot/app/commands/start.py
+++ tgbot/app/commands/start.py
@@ -2,6 +2,7 @@ from aiogram import Bot, Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
+import tgbot.app.utils
 from tgbot.app import i18n
 
 router = Router(name=__name__)
@@ -10,3 +11,10 @@ router = Router(name=__name__)
 @router.message(Command("start"))
 async def command(message: Message, bot: Bot) -> None:
     await bot.send_message(message.chat.id, i18n.t("start", message.chat.id))
+
+    await tgbot.app.utils.create_telegram_user(
+        message.chat.id,
+        message.chat.first_name or message.chat.full_name,
+        message.chat.last_name,
+        message.chat.username,
+    )
--- tgbot/app/utils.py
+++ tgbot/app/utils.py
@@ -1,6 +1,8 @@
 from aiogram.filters import Filter
 from aiogram.types import CallbackQuery
 
+import web.admin_app.telegram_bot.models
+
 
 class CallbackDataPrefixFilter(Filter):
     def __init__(self, prefix: str):
@@ -10,3 +12,25 @@ class CallbackDataPrefixFilter(Filter):
         if not callback.data:
             return False
         return callback.data.startswith(self.prefix)
+
+
+async def create_telegram_user(
+    chat_id: int,
+    first_name: str,
+    last_name: str | None = None,
+    username: str | None = None,
+):
+    if (
+        web.admin_app.telegram_bot.models.User.objects.filter(chat_id=chat_id)
+        .all()
+        .count()
+        >= 1
+    ):
+        return
+
+    await web.admin_app.telegram_bot.models.User(
+        chat_id=chat_id,
+        first_name=first_name,
+        last_name=last_name,
+        username=username,
+    ).asave()
